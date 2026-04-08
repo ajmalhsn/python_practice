bf46aba94829c9e7ef0773f6806b2559b7e4f411
Feroz Shaik <feroz.shaik@mheducation.com>
2026-04-08 00:19:01 -0400

    Variable Basics

---
+++ Basics/08_04_variables.py
@@ -0,0 +1,39 @@
+num1 = 200
+num2 = 300
+
+addition = num1 + num2
+print("The addition of num1 and num2 is: ", addition)
+print(type(addition))
+
+num3 = 10.5
+num4 = -20.5
+
+res1 = num3 + num4;
+print("The addition of num3 and num4 is: ", res1)
+print(type(res1))
+
+
+#string
+# collection of characters
+
+# "" (double quotes)  '' (single quotes)  """ """ (triple quotes)
+
+str1 = "Hello Python"
+str2 = 'Hello, Python Variables!!!'
+str3 = """
+        Hello Team
+        This is python multi line string
+        Time: 9AM
+            """
+
+print(str1)
+print(str2)
+print(str3)
+
+str = "Hello"
+print(str[0]) # first letter from left
+print(str[-1]) # first letter from right
+print(str[0:2]) # starts from 0 to 2 
+print(str[:2]) # starts from 0 to 2
+print(str[2:]) # starts from 2 till end
+print(str[::-1]) # reverse the string
